448bd9bfe8665aeefdb443b9302d0b2ba203e067
BertoSC <156660474+BertoSC@users.noreply.github.com>
2024-05-12 19:08:52 +0200

    Create Estudiante.java

---
+++ Estudiante.java
@@ -0,0 +1,17 @@
+import java.util.Queue;
+
+public class Estudiante extends Personaje{
+
+    public Estudiante(String aliasE) {
+        super(aliasE);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante [" + alias + "]";
+    }
+
+    
+
+}
